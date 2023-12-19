a602e9f3b4156af813767c2043de1ae85f94938a
Zuck <1105475412@eqq.com>
2023-12-19 19:08:25 +0800

    后端初始化

---
+++ bondNew/src/main/java/com/bond/bondnew/BondNewApplication.java
@@ -0,0 +1,14 @@
+package com.bond.bondnew;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BondNewApplication.class, args);
+    }
+
+}
---
+++ bondNew/src/test/java/com/bond/bondnew/BondNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bond.bondnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
